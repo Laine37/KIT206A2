07aed596aa4b7b7e89ed2e4313794ac004aebe42
balinp <balinp@utas.edu.au>
2022-03-30 13:38:18 +1100

    Initial

---
+++ Assignment2/Assignment2/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tut5
+{
+    class Employee
+    {
+        public string name;
+        public int id;
+        public gender gen;
+
+        public enum gender
+        {
+            M,
+            F,
+            X
+        }
+
+        public override string ToString()
+        {
+            return name + " " + id.ToString() + " " + gen.ToString();
+        }
+
+
+    }
+}
--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -4,13 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment2
+namespace Tut5
 {
     class Program
     {
+        static List<Employee> filterByGender(List<Employee> staff, Employee.gender gender)
+        {
+            List<Employee> returnList = new List<Employee>();
+
+            foreach (Employee item in staff)
+            {
+                if (item.gen == gender)
+                {
+                    returnList.Add(item);
+                }
+            }
+            return returnList;
+        }
+
         static void Main(string[] args)
         {
-            int i;
+            Employee emp1 = new Employee { name = "Jane", id = 1, gen = Employee.gender.F };
+            Employee emp2 = new Employee { name = "John", id = 2, gen = Employee.gender.M };
+            Employee emp3 = new Employee { name = "Billy", id = 3, gen = Employee.gender.M };
+            Employee emp4 = new Employee { name = "Anona", id = 4, gen = Employee.gender.X };
+            Employee emp5 = new Employee { name = "Liv", id = 5, gen = Employee.gender.F };
+
+            List<Employee> employees = new List<Employee>();
+
+            employees.Add(emp1);
+            employees.Add(emp2);
+            employees.Add(emp3);
+            employees.Add(emp4);
+            employees.Add(emp5);
+
+            foreach (Employee item in filterByGender(employees, Employee.gender.X))
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
         }
     }
 }
