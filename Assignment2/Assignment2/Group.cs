aac4df8ff25e40cfa70f1d4ddff0b7a1a4a78751
balinp <balinp@utas.edu.au>
2022-03-30 13:30:10 +1100

    Add student and group classes

---
+++ Assignment2/Assignment2/Group.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Group
+    {
+        private string groupName;
+        private string ID;
+
+        public Group(string name, string id)
+        {
+            //check database for same id if there is, 
+            {
+                //groupName = database.name;
+            }
+
+            groupName = name;
+            ID = id;
+        }
+
+        public bool setGroupName(string inName)
+        {
+            //goto database and set the group name
+
+            return false;//(groupNameFromDatabase == inName);
+        }
+        public string getGroupName(string ID)
+        {
+            return "Noname"; //get the name from the database
+        }
+    }
+}
